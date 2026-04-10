bdc55a005b8259dd5a1bc90ab796b3039e22b496
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-04-10 11:18:01 +0600

    created the book card and edit cards

---
+++ src/components/Ui/BookCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaRegStar } from "react-icons/fa";
+const BookCard = ({book}) => {
+    return (
+         <div className="card-body card bg-base-100 w-96 shadow-sm space-y-3 ">
+           
+              <figure className="p-7 rounded-xl bg-base-300">
+              <img
+                src={book.image}
+                alt= {book.bookName}
+                className=" h-[245px] rotate-10"
+              />
+            </figure>
+           
+            <div className="">
+              <h2 className="card-title mb-2">
+               {
+                book.tags.map((tag, index) => (
+                    <div key={index} className="badge badge-accent badge-soft">{tag}</div>
+                ))
+               }
+                
+              </h2>
+              <h2 className=" font-bold text-xl">  {book.bookName}</h2>
+              <p className="font-semibold text-lg">{book.author}</p>
+              
+              <div className="card-actions justify-between border-t border-dashed border-gray-300 pt-4 text-xl mt-4">
+
+                <div className="font-semibold ">{book.category}</div>
+                <div className=" font-semibold flex gap-2 items-center">{book.rating} <FaRegStar /></div>
+
+              </div>
+            </div>
+          </div>
+    );
+};
+
+export default BookCard;
--- src/pages/homePage/AllBooks.jsx
+++ src/pages/homePage/AllBooks.jsx
@@ -1,5 +1,7 @@
 import React, { use } from "react";
 
+import BookCard from "../../components/Ui/BookCard";
+
 const booksPromise = fetch("/booksData.json").then((res) => res.json());
 
 const AllBooks = () => {
@@ -9,32 +11,12 @@ const AllBooks = () => {
     <div className="my-12 container mx-auto">
       <h2 className="font-bold text-3xl text-center m-10"> Books</h2>
 
-      {books.map((book) => {
-        return (
-          <div className="card bg-base-100 w-96 shadow-sm ">
-            <figure>
-              <img
-                src={book.image}
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                <div className="badge badge-accent badge-soft ">Young Adult</div>
-                <div className="badge badge-accent badge-soft">Identity</div>
-              </h2>
-              <h2 className=" font-bold text-xl">  {book.bookName}</h2>
-              <p className="font-semibold text-lg">{book.author}</p>
-              
-              <div className="card-actions justify-end">
-                
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
-              </div>
-            </div>
-          </div>
-        );
+    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10  ">
+          {books.map((book,index) => {
+        return <BookCard key={index} book={book}></BookCard>;
+         
       })}
+    </div>
     </div>
   );
 };
